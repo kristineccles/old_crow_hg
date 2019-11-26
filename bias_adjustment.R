65385bce065043996248f6b7310d7b6d776fea76
Kristin Eccles <kristin.eccles@gmail.com>
2019-11-26 13:47:04 -0500

    Update bias_adjustment.R

--- bias_adjustment.R
+++ bias_adjustment.R
@@ -1,5 +1,7 @@
 ##########################################################
 # Adjust data for tree specific bias
+# Created in R 3.5.0
+# By: Kristin Eccles
 
 ##########################################################
 # Load Libraries
@@ -10,7 +12,6 @@ old_crow_raw=read.csv("old_crow_raw.csv", header=TRUE)
 oc_raw=old_crow_raw[,2:13]
 
 ##########################################################
-
 # Average Hg for each year (row) minus i using Tukey's robust biweighted mean
 
 tbw1=apply(oc_raw[-1], 1, function(x) TukeyBiweight(x, na.rm = TRUE))
@@ -38,29 +39,7 @@ colmean = colMeans(differences_oc, na.rm = TRUE, dims = 1)
 colmean
 
 # subtract raw value by mean difference of each tree
-oc_adjusted_data <- as.data.frame(t(apply(oc_raw, FUN=function(x) x - colmean, MARGIN = 1)))
+old_crow_adj = as.data.frame(t(apply(oc_raw, FUN=function(x) x - colmean, MARGIN = 1)))
 
 # Add year data
-oc_adjusted_data$Year=old_crow_raw$Year
-cbind(oc_adjusted_data)
-
-############################################
-# Site average -i
-# Average Hg for each year (row) minus i using Tukey's robust biweighted mean
-
-average1=apply(individual_tbwa[-1], 1, mean)
-average2=apply(individual_tbwa[-2], 1, mean)
-average3=apply(individual_tbwa[-3], 1, mean)
-average4=apply(individual_tbwa[-4], 1, mean)
-average5=apply(individual_tbwa[-5], 1, mean)
-average6=apply(individual_tbwa[-6], 1, mean)
-average7=apply(individual_tbwa[-7], 1, mean)
-average8=apply(individual_tbwa[-8], 1, mean)
-average9=apply(individual_tbwa[-9], 1, mean)
-average10=apply(individual_tbwa[-10], 1, mean)
-average11=apply(individual_tbwa[-11], 1, mean)
-average12=apply(individual_tbwa[-12], 1, mean)
-
-# Combine average dataset
-site_average_tbwa=as.data.frame(cbind(average1,average2, average3, average4, average5, average6, average7, average8, average9, average10, average11, average12))
-colnames(individual_tbwa)=colnames(individual_tbwa)
+old_crow_adj$Year=old_crow_raw$Year
